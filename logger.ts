33309a506e30523e92e1ae4a2ce90cf9767ff583
Densup <ggden22@gmail.com>
2023-04-27 12:36:09 +0300

    write logs to file

--- logger.ts
+++ logger.ts
@@ -1,16 +1,22 @@
-import log4js from "log4js";
+import log4js from 'log4js';
 log4js.configure({
   appenders: {
-    out: { type: 'stdout' },
+    file: {
+      type: 'file',
+      filename: 'logs/log.log',
+      backups: 3,
+      keepFileExt: true,
+      maxLogSize: '2M'
+    },
     app: { type: 'stdout' }
   },
   categories: {
-    default: { appenders: ['out'], level: 'debug' },
+    default: { appenders: ['file'], level: 'debug' },
     app: { appenders: ['app'], level: 'debug' }
   }
 });
 
-const appLogger = log4js.connectLogger( log4js.getLogger('app'), {} )
+const appLogger = log4js.connectLogger(log4js.getLogger('app'), {});
 const logger = log4js.getLogger();
 
 export { appLogger, logger };
